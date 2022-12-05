1b15c36065eac7ded25b62438c67641c1bb7dd66
udeaghad <dozie.udeagha@gmail.com>
2022-12-05 10:37:36 +0100

    Update desktop view

--- src/App.js
+++ src/App.js
@@ -16,9 +16,18 @@ const App = () => {
       source_code: 'https://github.com/udeaghad/group-project-hotel-reservation',
       demo: 'https://hotel-reservation-i2st.onrender.com/',
     },
-
     {
       id: 2,
+      name: 'Hotel Booking App',
+      description: 'This application is a web application that you can find your favorite Hotel around the world and reserve it for a specific date, find information about a hotel at details page and cancel a reservation.',
+      stack: ['NodeJS', 'ExpressJS', 'MongoDB', 'React', 'Redux'],
+      image: '/images/mern-hotel-api.png',
+      source_code: 'https://github.com/udeaghad/mern-hotel-api',
+      demo: 'https://booooka-hotel-app.netlify.app/',
+    },
+
+    {
+      id: 3,
       name: 'Budget App(Xpenda)',
       description: 'This Ruby on Rails project is about building a mobile web application to manage your budget: you have a list of transactions associated with a category so you can see how much money you spent and on what. I created an application allowing the user to register and log in so that the data is private.',
       stack: ['Ruby', 'Rails', 'Postgres', 'RSpec'],
@@ -29,7 +38,7 @@ const App = () => {
     },
 
     {
-      id: 3,
+      id: 4,
       name: 'Crypto Tracker',
       description: 'This project is about building a mobile web application to check a list of metrics (numeric values) which I created using React and Redux. I selected an API that provides numeric data about cryptocurrency exchange and then build the webapp around it',
       stack: ['React', 'Redux', 'Jest', 'JavaScript'],
@@ -39,7 +48,7 @@ const App = () => {
     },
 
     {
-      id: 4,
+      id: 5,
       name: 'Game of throne movie app',
       description: 'This project is about building a mobile web application to check a list of movies and their details using React and Redux. I selected an API that provides numeric data about movies and then build the webapp around it',
       stack: ['HTML', 'CSS', 'JavaScript', 'Webpack', 'Jest'],
@@ -49,7 +58,7 @@ const App = () => {
     },
 
     {
-      id: 5,
+      id: 6,
       name: "Space Travelers' Hub",
       description: 'In this Project, We worked with the real live data from the SpaceX API. We built a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.',
       stack: ['React', 'Redux', 'HTML/CSS', 'JavaScript'],
@@ -59,7 +68,7 @@ const App = () => {
     },
 
     {
-      id: 6,
+      id: 7,
       name: 'Maths Calculator',
       description: 'This project was done to learn the use React components, React props, and React state. It was later updated to Handle events in a React app, use React life cycle methods, and understand the mechanism of lifting state up. Also, React hook was used to refactor the components. Routing was also implemented for this SPA.',
       stack: ['HTML/CSS', 'JavaScript', 'React'],
@@ -67,17 +76,6 @@ const App = () => {
       source_code: 'https://github.com/udeaghad/my-maths-calculator',
       demo: 'https://my-maths-calculator.netlify.app/',
     },
-
-    {
-      id: 7,
-      name: 'Hotel Booking App',
-      description: 'This application is a web application that you can find your favorite Hotel around the world and reserve it for a specific date, find information about a hotel at details page and cancel a reservation.',
-      stack: ['NodeJS', 'ExpressJS', 'MongoDB', 'React', 'Redux'],
-      image: '/images/mern-hotel-api.png',
-      source_code: 'https://github.com/udeaghad/mern-hotel-api',
-      demo: 'https://booooka-hotel-app.netlify.app/',
-    },
-
   ];
 
   return (
