2608f207f8b5444ae658af9c0a931063a22af185
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-10-25 14:16:35 +0100

    Update projects-add pexel clone project

--- src/App.js
+++ src/App.js
@@ -17,6 +17,16 @@ const App = () => {
     },
     {
       id: 2,
+      name: 'pexels-clone',
+      description:
+      'In this poject, we have meticulously recreated key components of Pexels.com, encompassing features like the home, video, and upload pages. Utilizing advanced web techniques, I employed the JavaScript Intersection Observer for efficient API fetching and optimized user experience through lazy loading of images.',
+      stack: ['NEXTJS', 'JavaScript', 'Zustand', 'Intersection Observer API'],
+      image: '/images/pexel-thumbnail.png',
+      source_code: 'https://github.com/udeaghad/pexels-clone',
+      demo: 'https://pexels-clone-six.vercel.app/',
+    },
+    {
+      id: 3,
       name: 'Pick And Drop App',
       description:
       'This app enables companies/individuals in small-scale delivery businesses to subscribe, receive, monitor, and update orders received from their clients. The clients can also create orders on the app and select any company for delivery services.',
@@ -26,7 +36,7 @@ const App = () => {
       demo: 'https://pick-and-drop.onrender.com/api-docs',
     },
     {
-      id: 3,
+      id: 4,
       name: 'Dashboard',
       description:
       'This intuitive, responsive dashboard equips e-commerce businesses with tools to monitor sales metrics, interact with customers in real-time, and manage their profit margins.',
@@ -36,7 +46,7 @@ const App = () => {
       demo: 'https://metrix-gules.vercel.app/',
     },
     {
-      id: 4,
+      id: 5,
       name: 'Basic Bank App',
       description:
         'This fantastic product is specifically designed to handle all your basic payment needs, from transferring funds to receiving them and checking your transaction details and history. With our secure and reliable system, you can be assured of a hassle-free payment experience.',
@@ -46,7 +56,7 @@ const App = () => {
       demo: 'https://documenter.getpostman.com/view/22522406/2s93eSZF1B',
     },
     {
-      id: 5,
+      id: 6,
       name: 'Crypto Tracker',
       description:
         'This project is about building a mobile web application to check a list of metrics (numeric values) which I created using React and Redux. I selected an API that provides numeric data about cryptocurrency exchange and then developed the web app using the API.',
@@ -57,7 +67,7 @@ const App = () => {
     },
 
     {
-      id: 6,
+      id: 7,
       name: 'Game of throne movie app',
       description:
         'This project is about building a mobile web application to check a list of movies and their details using React and Redux. I selected an API that provides numeric data about movies and then built the web app around it.',
@@ -68,7 +78,7 @@ const App = () => {
     },
 
     {
-      id: 7,
+      id: 8,
       name: "Space Travelers' Hub",
       description:
         'In this Project, We worked with live data from the SpaceX API. We built a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.',
@@ -78,7 +88,7 @@ const App = () => {
       demo: 'https://galaxy-travellers.netlify.app/',
     },
     {
-      id: 8,
+      id: 9,
       name: 'Budget App(Xpenda)',
       description:
         'This Ruby on Rails project is about building a mobile web application to manage your budget: you have a list of transactions associated with a category and see how much money is spent and on what. I created an application allowing the user to register and log in so that the data is private.',
@@ -88,16 +98,6 @@ const App = () => {
       demo: 'https://xpenda-app.onrender.com/',
     },
 
-    {
-      id: 9,
-      name: 'Maths Calculator',
-      description:
-        'This project was to learn React components, React props, and React state. It was later updated to Handle events in a React app, use React life cycle methods, and understand the mechanism of the lifting state. Also, React hooks were used to refactor the components, and Routing was implemented for this SPA.',
-      stack: ['HTML/CSS', 'JavaScript', 'React'],
-      image: '/images/maths-calculator-app.png',
-      source_code: 'https://github.com/udeaghad/my-maths-calculator',
-      demo: 'https://my-maths-calculator.netlify.app/',
-    },
   ];
 
   return (
